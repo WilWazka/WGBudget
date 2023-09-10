6ce4c63f683290315d141aca57ddf3d98f4eecb2
Wilson Guasca <wil.wazka@gmail.com>
2023-09-10 12:56:30 -0500

    Add transaction entity.

---
+++ WGBudget.Entities/Data/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WGBudget.Entities.Data
+{
+    public class Transaction
+    {
+        [Key]
+        public int? ID { get; set; }
+        public int UserID { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string? Description { get; set; }
+        public decimal Amount { get; set; }
+        public int Category { get; set; }
+        public string TransactionType { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
